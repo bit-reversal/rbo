741e7f57d2caf62c8f0552e5aac782cc1a6eff71
Marcin Kik <mki1967@gmail.com>
2017-04-11 10:19:21 +0200

    package comment in doc.go

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+Implementation of the fundamental functions of the 
+Bit-reversal Broadcast Scheduling Algorithm RBO 
+(see:  https://sites.google.com/site/rboprotocol/ ).
+The most important here is the implementation 
+of the efficient  NSI (Next Slot In) Algorithm 
+that computes the next time slot in the broadcast cycle,
+when the receiver has to wake up and listen. 
+*/
+package rbo
